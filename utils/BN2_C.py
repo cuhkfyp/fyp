db87ec8fbf20cecf241ad18b9d864fe31782d328
final_year_project <94352630+cuhkfyp@users.noreply.github.com>
2022-03-17 09:16:04 +0800

    Add files via upload

--- utils/BN2_C.py
+++ utils/BN2_C.py
@@ -3,7 +3,7 @@ import math
 from numpy import linalg as LA
 from math import comb
 from collections import OrderedDict
-import torch
+
 import copy
 def cFinder(bc_arr, K, M):  #bc_arr => M
     ret_arr = []
@@ -13,17 +13,19 @@ def cFinder(bc_arr, K, M):  #bc_arr => M
         ret_arr.append(c)
     return ret_arr  #ret_arr => M
 
-def maxFinder(w_locals, K, C_arr):  #w_locals => M, C_arr => M
+def maxFinder(w_locals, K, C_arr,w_locals_real):  #w_locals => M, C_arr => M
     w = []
+    """
     for i in range(len(w_locals)):
         temp = []
         for item in w_locals[i].keys():
             temp = (np.concatenate((temp, np.array(w_locals[i][item].cpu().numpy())), axis=None))
         w.append(temp)
     l2_arr = LA.norm(np.array(w), axis=1)
-
+    """
+    l2_arr=np.array(w_locals)
     idxs = l2_arr.argsort()[::-1][:K]
-    ret_w = np.array(np.array(w_locals)[idxs])
+    ret_w = np.array(np.array(w_locals_real)[idxs])
     ret_l2 = np.array(l2_arr[idxs])
     ret_C = np.array(np.array(C_arr)[idxs])
     return (ret_w, ret_l2, ret_C)  #ret_w => K, ret_l2 => K, ret_C => K
@@ -147,10 +149,8 @@ def qFinder_1st(C_arr, new_w_locals, M, n,glob):
             else:
                 start = midpoint
             #print(midpoint)
-
-
         for item in new_w_locals[index].keys():
-            new_w_local_cpu=new_w_locals[index][item].cpu().numpy()
+            new_w_local_cpu = new_w_locals[index][item].cpu().numpy()
             glob_cpu = glob[item].cpu().numpy()
             k = new_w_local_cpu - glob_cpu
 
@@ -162,20 +162,8 @@ def qFinder_1st(C_arr, new_w_locals, M, n,glob):
         array_x = array_x[ind]
         max_ele = max(array_x)
         min_ele = min(array_x)
-        a = (max_ele - min_ele) / (2 ** 32)
 
-        array_x_temp = ((np.round((array_x - min_ele) / a)) * a) + min_ele
-
-        fin_result= copy.deepcopy(glob)
-
-
-        for i in range(target):
-            for key in new_w_locals[index].keys():
-                flag = ((new_w_locals[index][key] - glob[key] == array_x_temp[i]).nonzero()).cpu().numpy()
-                for j in flag:
-                    fin_result[key][tuple(j)] = fin_result[key][tuple(j)]+array_x_temp[i]
-        ret_arr.append(fin_result)
-
+        ret_arr.append(LA.norm(array_x))
 
 
 
